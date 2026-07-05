188680d6ea564b3c3e2457867f14a45c0d603a86
psychic-cyber <cyberofpsychics@gmail.com>
2026-07-05 13:57:17 +0500

    Hardhat Config File

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    amoy: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002,
+    },
+  },
 };
