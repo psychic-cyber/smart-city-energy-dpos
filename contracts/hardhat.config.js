36491fbd7c58e049ee668a98f304436e49269f5c
psychic-cyber <cyberofpsychics@gmail.com>
2026-07-10 15:13:54 +0500

    Deploy Hardhat on Localhost

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,13 +1,34 @@
+// require("@nomicfoundation/hardhat-toolbox");
+// require("dotenv").config();
+
+// module.exports = {
+//   solidity: "0.8.28",
+//   networks: {
+//     amoy: {
+//       url: process.env.RPC_URL,
+//       accounts: [process.env.PRIVATE_KEY],
+//       chainId: 80002,
+//     },
+//   },
+// };
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.28",
+
   networks: {
+
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+
     amoy: {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
       chainId: 80002,
     },
+
   },
 };
